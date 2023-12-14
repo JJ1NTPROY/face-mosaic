e6b28620f101087a3d5b313bf6386c5173865b18
eunjuuuuuuuuu <azsxdc3167@gachon.ac.kr>
2023-12-14 19:40:29 +0900

    eunju

--- src/face_mosaic.py
+++ src/face_mosaic.py
@@ -1,33 +1,50 @@
 import cv2
 
-def detect_and_mosaic_faces(image_path):
-    # 이미지 불러오기
-    image = cv2.imread(image_path)
-    
-    # 얼굴 감지를 위한 Haarcascade 사용
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # 얼굴 감지
-    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    
-    # 감지된 얼굴에 모자이크 처리
-    for (x, y, w, h) in faces:
-        face = image[y:y+h, x:x+w]
-        face = cv2.resize(face, (w//10, h//10))  # 모자이크 크기 조절
-        face = cv2.resize(face, (w, h), interpolation=cv2.INTER_AREA)
-        image[y:y+h, x:x+w] = face
-    
-    # 모자이크 처리된 이미지를 반환
-    return image
+cap = cv2.VideoCapture(0)
 
-if __name__ == "__main__":
-    # 이미지 경로 설정
-    input_image_path = "data/input/image.jpg"
-    output_image_path = "data/output/image_mosaic.jpg"
+if cap.isOpened():
+    file_path = 'video.mp4'
+    fps = 25.0
+    fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    size = (width, height)
 
-    # 얼굴 감지 및 모자이크 처리
-    result_image = detect_and_mosaic_faces(input_image_path)
+    out = cv2.VideoWriter(file_path, fourcc, fps, size)
 
-    # 결과 이미지 저장
-    cv2.imwrite(output_image_path, result_image)
+    recording = False
+
+    while True:
+        ret, frame = cap.read()
+
+        if ret:
+            cv2.imshow('camera-recording', frame)
+
+            key = cv2.waitKey(1)
+
+            # 'e' 키를 누르면 녹화 시작
+            if key == ord('e') and not recording:
+                recording = True
+                print("녹화 시작")
+            
+            # 'r' 키를 누르면 녹화 종료
+            elif key == ord('r') and recording:
+                recording = False
+                print("녹화 종료")
+
+            if recording:
+                out.write(frame)  # 파일에 프레임 저장
+
+            # 'q' 키를 누르면 종료
+            elif key == ord('q'):
+                break
+        else:
+            print('no frame')
+            break
+
+    out.release()  # 파일 닫기
+else:
+    print("카메라를 열 수 없습니다.")
+
+cap.release()
+cv2.destroyAllWindows()
